f27183e9abf46893001327f34ae05d959d35a731
Marco Caloba <123485381+MPCaloba@users.noreply.github.com>
2023-02-25 14:00:28 +0000

    Update CSV_to_Excel.py

--- CSV_to_Excel.py
+++ CSV_to_Excel.py
@@ -4,14 +4,10 @@ import glob
 from pathlib import Path
 
 # specifying the folder with the csv files
-path = "Divvy_2015"
+path = "(folder name)"
 
 # getting the csv files from the folder
 files = glob.glob(path + "/*.csv")
-
-# defining an empty list to store the content
-#data_frame = pd.DataFrame()
-#content = []
 
 # reading each csv file and exporting in xlsx format
 for filename in files:
@@ -24,8 +20,3 @@ for filename in files:
     
     # exporting the csv file in xlsx format
     pd.DataFrame(df).to_excel(newname + '.xlsx')
-    #content.append(df)
-
-# converting content to data frame
-#data_frame = pd.concat(content)
-#pd.DataFrame(data_frame).to_excel('2022.xlsx')
